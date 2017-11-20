aaf9e649ddad39c39825f0a5f072a9c06ac40d6a
Christophe Reymann <christophe.reymann@laas.fr>
2017-11-20 17:56:03 +0100

    Python versioning according to PEP440

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 import subprocess
 
 def get_version():
-    p = subprocess.run("git describe", shell=True, check=True,
+    p = subprocess.run("git describe | grep -o -E \"v[0-9]+(\\.[0-9]+)+(-[0-9]+)?\" -", shell=True, check=True,
                        universal_newlines=True, stdout=subprocess.PIPE)
-    return p.stdout.rstrip()
+    v = p.stdout.rstrip()
+    return v.replace("-", ".dev", 1).replace("v", "", 1)
 
 version = get_version()
 print("Package version: " + version)
